6675f23941485d34c025fbfca447817f29f2f8cc
Peiheng Su <thorkisu@gmail.com>
2025-06-18 23:18:42 +0100

    Add register and other forms for client

--- user/views.py
+++ user/views.py
@@ -1,50 +1,62 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+# from django.contrib import messages
+from .models import CustomUser, UserProfile, ClientProfile, VolunteerProfile
+from .forms import ClientRegisterForm, ClientProfileForm #, VolunteerRegisterForm, VolunteerProfileForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def home_view(request):
     return render(request, 'user/home.html')
 
 # The view of register
-def register_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password1 = request.POST.get('password1')
-        password2 = request.POST.get('password2')
+# def register_view(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         email = request.POST.get('email')
+#         password1 = request.POST.get('password1')
+#         password2 = request.POST.get('password2')
 
-        if password1 != password2:
-            messages.error(request, "The two passwords are inconsistent.")
-            return redirect('register')
+#         if password1 != password2:
+#             messages.error(request, "The two passwords are inconsistent.")
+#             return redirect('register')
 
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "The user name already exists.")
-            return redirect('register')
+#         if User.objects.filter(username=username).exists():
+#             messages.error(request, "The user name already exists.")
+#             return redirect('register')
 
-        user = User.objects.create_user(username=username, email=email, password=password1)
-        user.save()
-        messages.success(request, "Registration is successful, please log in.")
-        return redirect('login')
+#         user = User.objects.create_user(username=username, email=email, password=password1)
+#         user.save()
+#         messages.success(request, "Registration is successful, please log in.")
+#         return redirect('login')
 
-    return render(request, 'user/register.html')
+#     return render(request, 'user/register.html')
 
 
 #  The view of login
 def login_view(request):
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+
+    #     user = authenticate(request, username=username, password=password)
+    #     if user is not None:
+    #         login(request, user)
+    #         return redirect('dashboard')
+    #     else:
+    #         messages.error(request, "Wrong username or password.")
+    #         return redirect('login')
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
-            return redirect('dashboard')
-        else:
-            messages.error(request, "Wrong username or password.")
-            return redirect('login')
-
-    return render(request, 'user/login.html')
+            #return redirect('client_profile_detail')
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'user/login.html', {'form':form})
 
 
 # the view of log out
@@ -54,10 +66,41 @@ def logout_view(request):
 
 
 # the view of page
-def dashboard_view(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-    return render(request, 'user/dashboard.html', {'user': request.user})
+# def dashboard_view(request):
+#     if not request.user.is_authenticated:
+#         return redirect('login')
+#     return render(request, 'user/dashboard.html', {'user': request.user})
 
+def choose_role(request):
+    return render(request, 'user/role_choose.html')
 
+def client_register(request):
+    if request.method == 'POST':
+        form = ClientRegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('client_profile_edit')
+    else:
+        form = ClientRegisterForm()
+    return render(request, 'user/client_register.html', {'form':form})
 
+@login_required
+def client_profile_edit(request):
+    try:
+        client_profile = request.user.userprofile.clientprofile
+    except ClientProfile.DoesNotExist:
+        return redirect('client_profile_edit')
+    if request.method == 'POST':
+        form = ClientProfileForm(request.POST, request.FILES, instance=client_profile)
+        if form.is_valid():
+            form.save()
+            return redirect('client_profile_detail')
+    else:
+        form = ClientProfileForm(instance=client_profile)
+    return render(request, 'user/client_profile_edit.html', {'form':form})
+
+@login_required
+def client_profile_detail(request):
+    client_profile = request.user.userprofile.clientprofile
+    return render(request, 'user/client_profile_detail.html', {'client_profile':client_profile})
