848343cfd45bb17541eab39fb5dc07062a2c638b
Peiheng Su <thorkisu@gmail.com>
2025-07-04 22:54:09 +0100

    add time limitation for task, add cancel method in models

--- task/forms.py
+++ task/forms.py
@@ -3,6 +3,7 @@ from .models import Task
 from user.models import SupportType
 
 class TaskForm(forms.ModelForm):
+    vol_number = forms.IntegerField(min_value=1)
     work_area = forms.ModelMultipleChoiceField(
         queryset=SupportType.objects.all(),
         widget=forms.CheckboxSelectMultiple,
@@ -12,6 +13,6 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = ['title', 'description', 'start_time', 'end_time', 'vol_number', 'work_area']
         widgets = {
-            'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'start_time': forms.TextInput(attrs={'id': 'start_time'}),
+            'end_time': forms.TextInput(attrs={'id': 'end_time'}),
         }
