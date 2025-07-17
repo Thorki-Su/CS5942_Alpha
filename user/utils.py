a57262733c9e58fbdc52bb357be40e6d08769872
Peiheng Su <thorkisu@gmail.com>
2025-07-17 22:38:25 +0100

    change a better email content

--- user/utils.py
+++ user/utils.py
@@ -62,6 +62,13 @@ def geocode_address(address):
         print(f"[Geocode ERROR] 地址解析失败：{cleaned_address} → {e}")
     return None, None
 
+from django.core.mail import send_mail
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.urls import reverse
+from django.contrib.auth.tokens import default_token_generator
+
+
 def send_activation_email(user, request):
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
@@ -70,9 +77,23 @@ def send_activation_email(user, request):
         reverse('user:activate', kwargs={'uidb64': uid, 'token': token})
     )
 
-    subject = "Activate your account"
-    message = f"Hi {user.email},\n\nPlease click the link below to activate your account:\n\n{activation_url}\n\nIf you did not register, please ignore this email."
+    subject = "Activate Your Account"
+    message = f"""\
+Hi {user.first_name or user.email},
+
+Thank you for registering on our platform.
+
+To activate your account, please click the link below:
+
+{activation_url}
+
+If you did not sign up for this account, you can safely ignore this email.
+
+Best regards,  
+The Support Team
+"""
+
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [user.email]
 
-    send_mail(subject, message, from_email, recipient_list)
+    send_mail(subject, message, from_email, recipient_list, fail_silently=False)
