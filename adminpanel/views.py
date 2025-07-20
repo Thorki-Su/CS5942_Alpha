f8e5c8351978eb25f1aa5fb3c9d130c4785d0f19
Peiheng Su <thorkisu@gmail.com>
2025-07-20 16:23:55 +0100

    add task pages in adminpanel

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import get_user_model
+from task.models import Task, TaskApplication, TaskRecord, Feedback
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -14,12 +16,17 @@ def staff_required(view_func):
 
 @staff_required
 def admin_dashboard(request):
+    today = timezone.localdate()
     client_count = User.objects.filter(role='client').count()
     volunteer_count = User.objects.filter(role='volunteer').count()
+    task_count = Task.objects.count()
+    today_task_count = Task.objects.filter(created_at__date=today).count()
     
     context = {
         'client_count': client_count,
         'volunteer_count': volunteer_count,
+        'task_count': task_count,
+        'today_task_count': today_task_count,
     }
     return render(request, 'adminpanel/admin_dashboard.html', context)
 
@@ -32,3 +39,23 @@ def user_list(request):
 def user_detail(request, user_id):
     user = get_object_or_404(User, id=user_id)
     return render(request, 'adminpanel/user_detail.html', {'user': user})
+
+@staff_required
+def task_list(request):
+    tasks = Task.objects.all().order_by('id')
+    return render(request, 'adminpanel/task_list.html', {'tasks':tasks})
+
+@staff_required
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    applications = TaskApplication.objects.filter(task=task)
+    records = TaskRecord.objects.filter(task=task)
+    feedbacks = Feedback.objects.filter(task=task)
+
+    context = {
+        'task': task,
+        'applications': applications,
+        'records':records,
+        'feedbacks':feedbacks,
+    }
+    return render(request, 'adminpanel/task_detail.html', context)
