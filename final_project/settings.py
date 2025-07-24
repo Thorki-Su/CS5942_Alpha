be1d8b2bfba1fdf28f1304977ae3cc70576d84e5
qihang wan <qihangwan3@gmail.com>
2025-07-24 17:24:43 +0100

    1
    https://cs5942-alpha-test.onrender.com

--- final_project/settings.py
+++ final_project/settings.py
@@ -44,10 +44,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-i!o^^a8m_sz=(_5e_c07nyutwzr(fdu+uihy5=gpr^lwvwpotb'
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mangoairport-artistbanana-8000.codio-box.uk', 'cs5942-alpha.onrender.com','cs5942-alpha-test.onrender.com']
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    'mangoairport-artistbanana-8000.codio-box.uk', 
+    'cs5942-alpha.onrender.com',
+    'cs5942-alpha-test.onrender.com',
+    ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://cs5942-alpha.onrender.com',
+    'https://cs5942-alpha-test.onrender.com',
     'https://mangoairport-artistbanana-8000.codio-box.uk',
     'http://localhost',
     'http://localhost:8000',
